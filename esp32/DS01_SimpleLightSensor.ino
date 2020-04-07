fa8ea87a10ca535201d8e438ae8536d416fd153e
phanuwat1234 <63299089+phanuwat1234@users.noreply.github.com>
2020-04-07 20:44:23 +0700

    Update DS01_SimpleLightSensor.ino

--- esp32/DS01_SimpleLightSensor.ino
+++ esp32/DS01_SimpleLightSensor.ino
@@ -11,7 +11,7 @@ void setup(){
  Serial.println("program DS01");
  Serial.println("Simple Light Sensor");
  Serial.println("modified for esp32"); 
- Serial.println("modified by Digital Siam");
+ Serial.println("modified by Digital");
 }
 
 void loop(){
